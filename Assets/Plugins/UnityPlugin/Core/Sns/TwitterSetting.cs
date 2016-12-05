faa7c627a241b282698a7c612e89f03b2de89241
柾本 耕太郎 <kotaromasamoto@creation-2.local>
2016-12-05 11:34:19 +0900

    add annotation to unity plugin objects

--- Assets/Plugins/UnityPlugin/Core/Sns/TwitterSetting.cs
+++ Assets/Plugins/UnityPlugin/Core/Sns/TwitterSetting.cs
@@ -1,4 +1,13 @@
-﻿using UnityEngine;
+﻿//======================================================================
+// Project Name    : unity plugin
+//
+// Copyright © 2016 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+//======================================================================
+using UnityEngine;
 using System.Collections;
 namespace UnityPlugin.Core.Sns {
 public class TwitterSetting {
