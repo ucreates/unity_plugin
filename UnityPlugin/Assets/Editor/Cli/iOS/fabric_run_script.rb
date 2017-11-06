73dbf4bc57f8bf0222a2ffe49a006f411e15c4d6
柾本 耕太郎 <kotaromasamoto@creation-2.local>
2017-11-06 09:40:17 +0900

    improved build script for ios and formated some xmls

--- UnityPlugin/Assets/Editor/Cli/iOS/fabric_run_script.rb
+++ UnityPlugin/Assets/Editor/Cli/iOS/fabric_run_script.rb
@@ -7,14 +7,14 @@ buildSecret = ARGV[3]
 project = Xcodeproj::Project.open(projectPath)
 target = project.targets[0]
 for phase in target.build_phases do
-    if phase.respond_to?(:name)
-        if phase.name.to_s.downcase.include?("fabric")
-            target.build_phases.delete(phase)
-        end
-    end
+  next unless phase.respond_to?(:name)
+  if phase.name.to_s.downcase.include?('fabric')
+    target.build_phases.delete(phase)
+  end
 end
 phase = project.new(Xcodeproj::Project::Object::PBXShellScriptBuildPhase)
-phase.name = "Run Script for Fabric by Unity Twitter Plugin."
-phase.shell_script = frameworkPath + " " + apiKey + " " + buildSecret
+phase.name = 'Run Script for Fabric by Unity Twitter Plugin.'
+phase.shell_script = frameworkPath + ' ' + apiKey + ' ' + buildSecret
+phase.run_only_for_deployment_postprocessing = '1'
 target.build_phases.unshift(phase)
-project.save()
+project.save
